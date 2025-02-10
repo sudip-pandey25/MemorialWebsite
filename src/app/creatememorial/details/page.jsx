8a13d685a9b97849160baf387390932f463960cb
Sudip Shakya <mainsudip25@gmail.com>
2025-02-10 14:59:39 +0545

    details part

--- src/app/creatememorial/details/page.jsx
+++ src/app/creatememorial/details/page.jsx
@@ -16,14 +16,21 @@ const DetailsFormPage = () => {
   const [error, setError] = useState("");
   const router = useRouter();
   const [isClicked, setIsClicked] = useState(false);
-
-  const [formData, setFormData] = useState({
-    firstname: firstName,
-    middlename: middlename,
-    lastname: lastName,
-    gender: gender,
-    relationship: selectedRelationship,
-    designation: selectedDesignation,
+  const [BornDate, setBornDate] = useState({
+    day: "",
+    month: "",
+    year: "",
+    city: "",
+    country: "",
+    state: "",
+  });
+  const [demiseDate, setDemiseDate] = useState({
+    Dday: "",
+    Dmonth: "",
+    Dyear: "",
+    Dcity: "",
+    Dcountry: "",
+    Dstate: "",
   });
 
   const handleRelationshipChange = (e) => {
@@ -36,6 +43,16 @@ const DetailsFormPage = () => {
 
   const handleGenderChange = (e) => {
     setGender(e.target.value);
+  };
+
+  const handleChange = (e) => {
+    const { id, value } = e.target;
+    setBornDate({ ...BornDate, [id]: value });
+  };
+
+  const handleDChange = (e) => {
+    const { id, value } = e.target;
+    setDemiseDate({ ...demiseDate, [id]: value });
   };
 
   const handleSubmit = (e) => {
@@ -51,13 +68,24 @@ const DetailsFormPage = () => {
     ) {
       setError("Please fill out all required fields.");
     } else {
+      const formData = {
+        firstname: firstName,
+        middlename: middlename,
+        lastname: lastName,
+        gender: gender,
+        relationship: selectedRelationship,
+        designation: selectedDesignation,
+        BornDate,
+        demiseDate,
+      };
       router.push(`/creatememorial/pricing`);
+      console.log(formData);
       setError("");
     }
   };
 
   return (
-    <div className="h-fit px-4 md:px-6 lg:px-8 xl:px-12 py-4">
+    <div className="h-fit px-4 md:px-6 lg:px-8 xl:px-12 py-4 w-full">
       <p className="font-semibold font-lato text-xl md:text-3xl text-primary">
         This Memorial is Dedicated to:
       </p>
@@ -66,9 +94,11 @@ const DetailsFormPage = () => {
       {error && <p className="text-red-500">{error}</p>}
 
       <form
-        className="grid grid-cols-1 md:grid-cols-2 py-2 md:py-8 gap-2 md:gap-6"
+        className="grid grid-cols-1 md:grid-cols-2 py-2 md:py-8 gap-2 md:gap-6 w-full"
         onSubmit={handleSubmit}
       >
+        {/* Required Info */}
+
         <div className="name col-span-1">
           <label
             htmlFor="name"
@@ -223,15 +253,187 @@ const DetailsFormPage = () => {
           </div>
         </div>
 
+        {/* Additional Info */}
+
         <div
           className={`${
             isClicked
-              ? "opacity-100 translate-y-0 visible"
-              : "opacity-0 -translate-y-4 invisible"
-          } transition-all duration-300 ease-in-out transform md:col-span-2 additional-info bg-foreground rounded-lg`}
+              ? "opacity-100 translate-y-0 block"
+              : "opacity-0 -translate-y-4  hidden"
+          } transition-all duration-300 ease-in-out transform md:col-span-2 additional-info bg-foreground rounded-lg text-white w-full`}
         >
-          <div className="px-4 py-4">
-            This Information can also be updated at a later time:
+          <div className="px-4 py-4 grid grid-cols-1  font-lato w-full gap-2 md:gap-4">
+            <p className=" text-base md:text-xl md:col-span-2 pb-3">
+              This Information can also be updated at a later time:
+            </p>
+            {/* Born Info */}
+            <div className=" md:col-span-2 md:w-3/4 grid grid-cols-1 md:grid-cols-4 gap-4 items-center">
+              <label htmlFor="birthDay" className="font-semibold text-base">
+                Born Info:
+              </label>
+              {/* Day Select */}
+              <select
+                id="day"
+                name="day"
+                value={BornDate.day}
+                onChange={handleChange}
+                className="px-2 py-1 border border-gray-300 rounded-md text-base text-primary focus:outline-none focus:ring-2 focus:ring-primary"
+              >
+                <option value="" disabled>
+                  DD
+                </option>
+                {Array.from({ length: 31 }, (_, i) => i + 1).map((day) => (
+                  <option key={day} value={day}>
+                    {day < 10 ? `0${day}` : day}
+                  </option>
+                ))}
+              </select>
+
+              {/* Month Select */}
+              <select
+                id="month"
+                name="month"
+                value={BornDate.month}
+                onChange={handleChange}
+                className=" px-2 py-1 border border-gray-300 rounded-md text-base text-primary focus:outline-none focus:ring-2 focus:ring-primary"
+              >
+                <option value="" disabled>
+                  MM
+                </option>
+                {Array.from({ length: 12 }, (_, i) => i + 1).map((month) => (
+                  <option key={month} value={month}>
+                    {month < 10 ? `0${month}` : month}
+                  </option>
+                ))}
+              </select>
+
+              {/* Year Select */}
+              <select
+                id="year"
+                name="year"
+                value={BornDate.year}
+                onChange={handleChange}
+                className=" px-2 py-1 border border-gray-300 rounded-md text-base text-primary focus:outline-none focus:ring-2 focus:ring-primary"
+              >
+                <option value="" disabled>
+                  YYYY
+                </option>
+                {Array.from({ length: 101 }, (_, i) => 1923 + i).map((year) => (
+                  <option key={year} value={year}>
+                    {year}
+                  </option>
+                ))}
+              </select>
+              <div></div>
+              <input
+                id="city"
+                type="text"
+                value={BornDate.city}
+                onChange={handleChange}
+                placeholder="City or Town"
+                className="px-2 py-1 border border-gray-300 rounded-md text-base text-primary focus:outline-none focus:ring-2 focus:ring-primary"
+              />
+              <input
+                id="country"
+                type="text"
+                value={BornDate.country}
+                onChange={handleChange}
+                placeholder="Country"
+                className="px-2 py-1 border border-gray-300 rounded-md text-base text-primary focus:outline-none focus:ring-2 focus:ring-primary"
+              />
+              <input
+                type="text"
+                id="state"
+                value={BornDate.state}
+                onChange={handleChange}
+                placeholder="State or Area"
+                className="px-2 py-1 border border-gray-300 rounded-md text-base text-primary focus:outline-none focus:ring-2 focus:ring-primary"
+              />
+            </div>
+            {/* Demise info */}
+            <div className=" md:col-span-2 md:w-3/4 grid grid-cols-1 md:grid-cols-4 gap-4 items-center">
+              <label htmlFor="birthDay" className="font-semibold text-base">
+                Demise Info:
+              </label>
+              {/* Day Select */}
+              <select
+                id="Dday"
+                value={demiseDate.Dday}
+                onChange={handleDChange}
+                name="day"
+                className="px-2 py-1 border border-gray-300 rounded-md text-base text-primary focus:outline-none focus:ring-2 focus:ring-primary"
+              >
+                <option value="" disabled>
+                  DD
+                </option>
+                {Array.from({ length: 31 }, (_, i) => i + 1).map((day) => (
+                  <option key={day} value={day}>
+                    {day < 10 ? `0${day}` : day}
+                  </option>
+                ))}
+              </select>
+
+              {/* Month Select */}
+              <select
+                id="Dmonth"
+                name="month"
+                value={demiseDate.Dmonth}
+                onChange={handleDChange}
+                className=" px-2 py-1 border border-gray-300 rounded-md text-base text-primary focus:outline-none focus:ring-2 focus:ring-primary"
+              >
+                <option value="" disabled>
+                  MM
+                </option>
+                {Array.from({ length: 12 }, (_, i) => i + 1).map((month) => (
+                  <option key={month} value={month}>
+                    {month < 10 ? `0${month}` : month}
+                  </option>
+                ))}
+              </select>
+
+              {/* Year Select */}
+              <select
+                id="Dyear"
+                name="year"
+                value={demiseDate.Dyear}
+                onChange={handleDChange}
+                className=" px-2 py-1 border border-gray-300 rounded-md text-base text-primary focus:outline-none focus:ring-2 focus:ring-primary"
+              >
+                <option value="" disabled>
+                  YYYY
+                </option>
+                {Array.from({ length: 101 }, (_, i) => 1923 + i).map((year) => (
+                  <option key={year} value={year}>
+                    {year}
+                  </option>
+                ))}
+              </select>
+              <div></div>
+              <input
+                type="text"
+                id="Dcity"
+                value={demiseDate.Dcity}
+                onChange={handleDChange}
+                placeholder="City or Town"
+                className="px-2 py-1 border border-gray-300 rounded-md text-base text-primary focus:outline-none focus:ring-2 focus:ring-primary"
+              />
+              <input
+                type="text"
+                id="Dcountry"
+                value={demiseDate.Dcountry}
+                onChange={handleDChange}
+                placeholder="Country"
+                className="px-2 py-1 border border-gray-300 rounded-md text-base text-primary focus:outline-none focus:ring-2 focus:ring-primary"
+              />
+              <input
+                type="text"
+                id="Dstate"
+                value={demiseDate.Dstate}
+                onChange={handleDChange}
+                placeholder="State or Area"
+                className="px-2 py-1 border border-gray-300 rounded-md text-base text-primary focus:outline-none focus:ring-2 focus:ring-primary"
+              />
+            </div>
           </div>
         </div>
 
