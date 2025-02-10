9332f1af0d3b970e42256834acec2fe10827ea5d
Sudip Shakya <mainsudip25@gmail.com>
2025-02-10 12:04:04 +0545

    Suspense fix

--- src/app/creatememorial/details/page.jsx
+++ src/app/creatememorial/details/page.jsx
@@ -3,21 +3,28 @@ import Link from "next/link";
 import { useSearchParams } from "next/navigation";
 import React, { Suspense } from "react";
 
-const page = () => {
+const DetailsFormPage = () => {
   const searchParams = useSearchParams();
-  const firstName = searchParams.get("firstName");
-  const lastName = searchParams.get("lastName");
+  const firstName = searchParams?.get("firstName");
+  const lastName = searchParams?.get("lastName");
+
   return (
-    <Suspense>
-      <div>
-        firstName: {firstName}
-        Details form page
-        <form action="">
-          <Link href="/creatememorial/pricing">
-            <button>Choose Pricing</button>
-          </Link>
-        </form>
-      </div>
+    <div>
+      <p>First Name: {firstName || "N/A"}</p>
+      <p>Last Name: {lastName || "N/A"}</p>
+      <form>
+        <Link href="/creatememorial/pricing">
+          <button type="button">Choose Pricing</button>
+        </Link>
+      </form>
+    </div>
+  );
+};
+
+const page = () => {
+  return (
+    <Suspense fallback={<div>Loading form details...</div>}>
+      <DetailsFormPage />
     </Suspense>
   );
 };
