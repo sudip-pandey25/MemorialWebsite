6b874ec7963b3a3441394cd05a8f680043402aba
Sudip Shakya <mainsudip25@gmail.com>
2025-02-10 11:53:08 +0545

    Register page setup

--- src/app/components/creatememorial/Heading.jsx
+++ src/app/components/creatememorial/Heading.jsx
@@ -1,26 +1,48 @@
 import React from "react";
+import { motion } from "framer-motion";
 
-const Heading = () => {
+const Heading = ({ active }) => {
+  const activeClassName = "text-foreground border-foreground font-semibold";
+  const activeSpan =
+    "border bg-primary rounded-full text-white w-5 h-5 lg:w-8 lg:h-8";
+
+  const headingVariants = {
+    initial: { opacity: 0.5, scale: 0.9 },
+    active: { opacity: 1, scale: 1 },
+  };
+
   return (
-    <div className="flex gap-12 justify-center items-center text-center my-10 w-full text-xs lg:text-base font-poppins text-gray-700 ">
-      <div className="heading-1 flex flex-col items-center justify-center pb-2 border-b-2 text-foreground border-foreground font-semibold flex-1">
-        <span className="flex items-center justify-center border bg-primary rounded-full text-white w-5 h-5 lg:w-8 lg:h-8">
-          1
-        </span>
-        <h1>Account Details</h1>
-      </div>
-      <div className="heading-1 flex flex-col items-center justify-center flex-1">
-        <span>2</span>
-        <h1>About Loved Ones</h1>
-      </div>
-      <div className="heading-1 flex flex-col items-center justify-center flex-1">
-        <span>3</span>
-        <h1>Choose Plans</h1>
-      </div>
-      <div className="heading-1 flex flex-col items-center justify-center flex-1">
-        <span>4</span>
-        <h1>Privacy options</h1>
-      </div>
+    <div className="flex gap-12 justify-center items-center text-center my-10 w-full text-xs lg:text-base font-poppins text-gray-700 transition-all duration-300 ease-in-out">
+      {[
+        "Account Details",
+        "About Loved Ones",
+        "Pricing",
+        "Privacy Settings",
+      ].map((title, index) => {
+        const isActive = active === index + 1;
+
+        return (
+          <motion.div
+            key={index}
+            variants={headingVariants}
+            initial="initial"
+            animate={isActive ? "active" : "initial"}
+            className={`heading-1 flex flex-col items-center justify-center pb-2 border-b-2 flex-1 ${
+              isActive ? `${activeClassName}` : ""
+            }`}
+            transition={{ duration: 0.3 }}
+          >
+            <motion.span
+              className={`flex items-center justify-center ${
+                isActive ? `${activeSpan}` : ""
+              }`}
+            >
+              {index + 1}
+            </motion.span>
+            <h1>{title}</h1>
+          </motion.div>
+        );
+      })}
     </div>
   );
 };
--- src/app/creatememorial/register/page.jsx
+++ src/app/creatememorial/register/page.jsx
@@ -1,33 +1,150 @@
 "use client";
-import Link from "next/link";
-import React from "react";
 
-const page = () => {
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
+
+const Page = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const router = useRouter();
+
+  const [formErrors, setFormErrors] = useState({});
+
+  const handleChange = (e) => {
+    const { id, value } = e.target;
+    setFormData({ ...formData, [id]: value });
+  };
+
+  const validateForm = () => {
+    let errors = {};
+
+    if (!formData.name) errors.name = "Name is required";
+    if (!formData.email) errors.email = "Email is required";
+    if (!formData.password) errors.password = "Password is required";
+    if (!formData.confirmPassword)
+      errors.confirmPassword = "Confirm password is required";
+    if (formData.password !== formData.confirmPassword) {
+      errors.confirmPassword = "Passwords do not match";
+    }
+
+    setFormErrors(errors);
+    return Object.keys(errors).length === 0;
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (validateForm()) {
+      router.push(`/creatememorial/details`);
+      console.log("Form is valid, proceed!");
+    }
+  };
+
   return (
-    <div className="h-[70vh] px-4 md:px-6 lg:px-8 xl:px-12 py-4">
-      <input
-        type="text"
-        // onChange={(e) => setFirstName(e.target.value)}
-        className="border border-purple-200"
-        placeholder="First Name"
-      />
-
-      <input
-        type="text"
-        // onChange={(e) => setLastName(e.target.value)}
-        className="border border-purple-200"
-        placeholder="Last Name"
-      />
-
-      <Link
-        href={{
-          pathname: "/creatememorial/details",
-        }}
+    <div className="h-fit px-4 md:px-6 lg:px-8 xl:px-12 py-4 font-lato">
+      <p className="py-2 text-2xl font-medium text-primary md:text-3xl">
+        Your Details
+      </p>
+      <p className="text-xs md:text-base text-gray-600 italic">
+        Already have an account?{" "}
+        <span className="text-primary underline not-italic ">Sign in</span>
+      </p>
+      <form
+        onSubmit={handleSubmit}
+        className="py-2 grid grid-cols-1 md:grid-cols-2 gap-2 md:gap-4"
       >
-        <button>Continue</button>
-      </Link>
+        <div>
+          <label
+            htmlFor="name"
+            className="text-xs md:text-base mb-2 text-gray-600"
+          >
+            Name:
+          </label>
+          <input
+            type="text"
+            id="name"
+            value={formData.name}
+            onChange={handleChange}
+            placeholder="Name"
+            className="border border-gray-400 outline-none text-base font-poppins px-2 py-3 focus:border-b focus:border-primary transition-all duration-300 ease-in-out w-full focus:bg-primary focus:text-white rounded-lg placeholder:italic placeholder:text-xs"
+          />
+          {formErrors.name && (
+            <p className="text-red-500 text-xs">{formErrors.name}</p>
+          )}
+        </div>
+        <div>
+          <label
+            htmlFor="email"
+            className="text-xs md:text-base pb-2 text-gray-600"
+          >
+            Email:
+          </label>
+          <input
+            type="email"
+            id="email"
+            value={formData.email}
+            onChange={handleChange}
+            placeholder="Enter your email"
+            className="border border-gray-400 outline-none text-base font-poppins px-2 py-3 focus:border-b focus:border-primary transition-all duration-300 ease-in-out w-full focus:bg-primary focus:text-white rounded-lg placeholder:italic placeholder:text-xs"
+          />
+          {formErrors.email && (
+            <p className="text-red-500 text-xs">{formErrors.email}</p>
+          )}
+        </div>
+        <div>
+          <label
+            htmlFor="password"
+            className="text-xs md:text-base pb-2 text-gray-600"
+          >
+            Password:
+          </label>
+          <input
+            type="password"
+            id="password"
+            value={formData.password}
+            onChange={handleChange}
+            placeholder="Enter your password"
+            className="border border-gray-400 outline-none text-base font-poppins px-2 py-3 focus:border-b focus:border-primary transition-all duration-300 ease-in-out w-full focus:bg-primary focus:text-white rounded-lg placeholder:italic placeholder:text-xs"
+          />
+          {formErrors.password && (
+            <p className="text-red-500 text-xs">{formErrors.password}</p>
+          )}
+        </div>
+        <div>
+          <label
+            htmlFor="confirmPassword"
+            className="text-xs pb-2 text-gray-600 md:text-base"
+          >
+            Confirm Password:
+          </label>
+          <input
+            type="password"
+            id="confirmPassword"
+            value={formData.confirmPassword}
+            onChange={handleChange}
+            placeholder="Retype your password"
+            className="border border-gray-400 outline-none text-base font-poppins px-2 py-3 focus:border-b focus:border-primary transition-all duration-300 ease-in-out w-full focus:bg-primary focus:text-white rounded-lg placeholder:italic placeholder:text-xs"
+          />
+          {formErrors.confirmPassword && (
+            <p className="text-red-500 text-xs">{formErrors.confirmPassword}</p>
+          )}
+        </div>
+        <div></div>
+        <div className="flex flex-row justify-end">
+          <button
+            type="submit"
+            className="bg-foreground px-3 py-2 rounded-full hover:px-4  transition-all duration-300 ease-in-out hover:shadow-sm text-white"
+          >
+            Continue
+          </button>
+        </div>
+      </form>
     </div>
   );
 };
 
-export default page;
+export default Page;
