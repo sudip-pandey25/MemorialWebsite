5a42ffcf3bdc9dbc8afc6fcdddc6f005b226a317
Sudip Shakya <mainsudip25@gmail.com>
2025-02-10 11:58:32 +0545

    Suspense added

--- src/app/creatememorial/details/page.jsx
+++ src/app/creatememorial/details/page.jsx
@@ -1,22 +1,24 @@
 "use client";
 import Link from "next/link";
 import { useSearchParams } from "next/navigation";
-import React from "react";
+import React, { Suspense } from "react";
 
 const page = () => {
   const searchParams = useSearchParams();
   const firstName = searchParams.get("firstName");
   const lastName = searchParams.get("lastName");
   return (
-    <div>
-      firstName: {firstName}
-      Details form page
-      <form action="">
-        <Link href="/creatememorial/pricing">
-          <button>Choose Pricing</button>
-        </Link>
-      </form>
-    </div>
+    <Suspense>
+      <div>
+        firstName: {firstName}
+        Details form page
+        <form action="">
+          <Link href="/creatememorial/pricing">
+            <button>Choose Pricing</button>
+          </Link>
+        </form>
+      </div>
+    </Suspense>
   );
 };
 
